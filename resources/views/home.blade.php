aada798c3e9b9a44530159b2e9402ef895c672e6
dankukennedy <dankukennedy@gmail.com>
2024-11-15 13:45:27 +0000

    my first commit on laravel framework

---
+++ resources/views/home.blade.php
@@ -0,0 +1,68 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    @auth 
+    <p>Congrats you are logged in</p>
+    <form action="/logout" method="POST">
+    @csrf
+    <button>Log Out</button>
+    </form>
+
+    <div style="border: 3px solid blue; padding:2px;  ">
+        <h2>Create a New Post</h2>
+        <form action="/create-post" method="POST">
+            @csrf
+            <input type="text" name="title" placeholder="Title">
+            <textarea name="body" placeholder="body content...."></textarea>
+            <button>Save Post</button>
+        </form>
+    </div>
+
+    <div style="border: 3px solid blue; padding:2px;  ">
+         <h2>All Posts</h2>
+         @foreach ($posts as $post)
+         <div style="background-color:gray; padding:10px; margin:10px">
+              <h3>{{$post['title']}}</h3>
+              {{$post['body']}}
+              <p><a href="/edit-post/{{$post->id}}">Edit</a></p>
+              <form action="/delete-post/{{$post->id}}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button>Delete</button>
+              </form>
+         </div>
+         @endforeach
+    </div>
+
+    @else
+    
+    <div style="border: 3px solid blue; padding:2px;  ">
+        <h2>Register</h2>
+        <form action="/register" method="POST">
+           @csrf
+           <input type="text" name="name" placeholder="name">
+           <input type="text" name="email" placeholder="email">
+           <input type="password" name="password" placeholder="password">
+           <button>Register</button>
+        </form>
+    </div>
+
+    <div style="border: 3px solid blue; padding:2px; margin-top:20px  ">
+        <h2>Login</h2>
+        <form action="/login" method="POST">
+           @csrf
+           <input type="text" name="loginname" placeholder="name">
+           <input type="password" name="loginpassword" placeholder="password">
+           <button>Login</button>
+        </form>
+    </div>
+    @endauth
+    
+</body>
+</html>
