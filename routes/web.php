fbc9e344a0a23f58c6c5a5d47829530dd66366d5
dankukennedy <dankukennedy@gmail.com>
2024-11-15 14:51:04 +0000

    editting post function

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::get('/', function () {
     return view('home',['posts'=>$posts]);
 });
 
+//Functionality Route
 Route::post('/register',[UserController::class,'register']);
 Route::post('/logout',[UserController::class,'logout']);
 Route::post('/login',[UserController::class,'login']);
@@ -23,3 +24,4 @@ Route::post('/login',[UserController::class,'login']);
 Route::post('/create-post',[PostController::class,'createPost']);
 Route::get('/edit-post/{post}',[PostController::class,'showEditScreen']);
 Route::put('/edit-post/{post}',[PostController::class,'actuallyUpdatePost']);
+Route::delete('/delete-post/{post}',[PostController::class,'deletePost']);
